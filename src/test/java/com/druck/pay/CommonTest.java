dfdbf82b334681b8979ac57b5c57d624d03c1c06
772010988@qq.com <772010988@qq.com>
2017-09-03 10:04:16 +0800

    提交到本地

--- src/test/java/com/druck/pay/CommonTest.java
+++ src/test/java/com/druck/pay/CommonTest.java
@@ -12,12 +12,7 @@ public class CommonTest {
 			System.out.println("============>"+str);
 		} else {
 			System.out.println("============>"+null);
-			System.out.println("============>2");
 			System.out.println("============>"+1);
-			System.out.println("============>"+4);
-			System.out.println("============>"+3);
-			System.out.println("============>"+6);
-			System.out.println("============>"+5);
 		}
 	}
 }
